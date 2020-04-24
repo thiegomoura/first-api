c349ff43651a803ac3022b078f2ccb830f9cd663
thiegomoura <thi-ego@hotmail.com>
2020-04-23 23:43:17 -0400

    alteracao conexao

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 //iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://thiego:14dms%23Afz@storage-zr8pm.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 requireDir ('./src/models');
 
